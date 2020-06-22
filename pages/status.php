be9528ee4ec808fc49d6f869c966c929591b7750
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2020-06-22 19:25:04 +0200

    add jquery refresher to server status

--- pages/status.php
+++ pages/status.php
@@ -1,6 +1,23 @@
 <h1 class="title"><?php print $pageHeaders['status']; ?></h1>
-<div class="container">
+<div class="container" id="viewerContainer">
 <?php
-echo $ts3_VirtualServer->getViewer(new TeamSpeak3_Viewer_Html());
+print $ts3_VirtualServer->getViewer(new TeamSpeak3_Viewer_Html());
 ?>
+<p id="refreshText"><progress class="progress is-medium is-dark" max="100"></progress></p>
 </div>
+
+<script>
+    var interval = 60;
+    $(document).ready(
+            function() {
+                setInterval(function(){
+                $('#refreshText').addClass('tag is-dark is-medium');
+                $('#refreshText').text('Refrešuju status za: ' + interval);
+                interval--;
+                if(interval <= 0){ interval = 60; }
+                }, 1000);
+                setInterval(function() {
+                    location.reload(); //refresh every minute
+                }, 60 * 1000);
+            });
+</script>
