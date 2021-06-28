16ef12458da8557c89ddceb2eadb877eff12892e
Dominik Zarsky <dominik.zarsky@gmail.com>
2021-06-28 21:34:28 +0200

    parametrize api url, formatting

--- pages/cfg/bot.php
+++ pages/cfg/bot.php
@@ -26,33 +26,43 @@ class Bot {
     }
 
     public function command($type, $arg) {
+
         $ch = $this->init();
         $typeraw = rawurlencode($type);
         $argraw = rawurlencode($arg);
         if ($arg != '0') curl_setopt($ch, CURLOPT_URL, 'http://' . $this->env['server_ip'] . ':' . $this->env['ts3audibot_port'] . '/api/bot/use/0(/' . $typeraw . '/' . $argraw); 
-        else curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:58913/api/bot/use/0(/' . $typeraw . '/)');
-        try {
+        else curl_setopt($ch, CURLOPT_URL, 'http://' . $this->env['server_ip'] . ':' . $this->env['ts3audibot_port'] . '/api/bot/use/0(/' . $typeraw . '/)');
+        try 
+        {
             curl_exec($ch);
-        } catch (Exception $e) {
+        } 
+        catch (Exception $e) 
+        {
             print 'An error has occured. Exception ' . $e->getMessage();
         }
 
         curl_close($ch);
+
     }
 
     public function status() {
+
         $ch = $this->init();
         curl_setopt($ch, CURLOPT_URL, 'http://' . $this->env['server_ip'] . ':' . $this->env['ts3audibot_port'] . '/api/bot/use/0(/song');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            try {
-                $final = curl_exec($ch);
-                $json = json_decode($final,true);
-                if ($json['Title']) print $json['Title']; else print 'Bot is inactive';
-            } catch (Exception $e) {
-                print 'An error has occured. Exception: ' . $e->getMessage();
-            }
+        try 
+        {
+            $final = curl_exec($ch);
+            $json = json_decode($final,true);
+            if ($json['Title']) print $json['Title']; else print 'Bot is inactive';
+        } 
+        catch (Exception $e) 
+        {
+            print 'An error has occured. Exception: ' . $e->getMessage();
+        }
 
-            curl_close($ch);
+        curl_close($ch);
+
     }
 
 }
